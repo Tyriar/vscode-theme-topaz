5493319ff9c2b96a407fa049b5a60481be9adedb
Daniel Imms <daimms@microsoft.com>
2019-05-04 09:01:24 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,91 @@
+## 0.8.2
+
+- Add remote status bar color
+- Add active indent background
+
+## 0.8.1
+
+- Improve contrast of text and diff editor inserted/deleted background colors [#2](https://github.com/Tyriar/vscode-theme-sapphire/issues/2) via [@Tyriar](https://github.com/Tyriar)
+
+## 0.8.0
+
+- Upgraded to vscode-theme-generator@0.0.21
+- Highlight modified tabs is now supported (add setting: `"workbench.editor.highlightModifiedTabs": true`), via [@rdnlsmith](https://github.com/rdnlsmith)
+- String escapes are now displayed slightly differently to the regular string color, via [@rdnlsmith](https://github.com/rdnlsmith)
+
+## 0.7.0
+
+- Upgraded to vscode-theme-generator@0.0.19
+- Template expression and embedded language colors are now reset correctly
+- Added support for empty editor group focus border (coming in VS Code v1.25)
+
+## 0.6.0
+
+- Upgraded to vscode-theme-generator@0.0.18, special thanks to [@usernamehw](https://github.com/usernamehw) for many issue reports
+- TS/JS: Class members of form `class A { a = () => 0; }` are now colored
+- TS/JS: Fixed template expression `${}` coloring
+- TS/JS: Fixed bracket within template expression coloring
+- Markdown: Added code block and inline code colors
+
+## 0.5.0
+
+- Added a new icon
+
+## 0.4.0
+
+- Added gutter SCM decoration colors
+- Added explorer SCM and error colors
+
+## 0.3.0
+
+- Added non-editor selection colors
+- Added tab border color
+- Added active line number color
+- Lighten comment foreground color to help contrast with the selection
+
+## 0.2.1
+
+- Added dim and bright variants
+
+## 0.2.0
+
+- Upgraded to vscode-theme-generator@0.0.13 designed for VS Code 1.12, this added support for theming many new workbench components
+- Background: Made slightly lighter
+
+## 0.1.1
+
+- Clean up readme
+
+## 0.1.0
+
+- Background: Made slightly darker
+- Added workbench support
+- Added terminal support
+- Many other changes from upgrading underlying [theme generator](https://github.com/Tyriar/vscode-theme-generator)
+
+## 0.0.6
+
+- Move release notes from README.md to CHANGELOG.md
+
+## 0.0.5
+
+- Markdown: Make bold and italic light blue
+- Markdown: Make list items blue
+
+## 0.0.4
+
+- C#: Make namespaces default white
+- C#: Make preprocessors the comment color
+
+## 0.0.3
+
+- Add basic C# support
+
+## 0.0.2
+
+- Added a color specific for block quotes in markdown
+- Lightened the comment color so that it has slightly better contrast with the diff background [#1](https://github.com/Tyriar/vscode-theme-sapphire/issues/1)
+
+## 0.0.1
+
+Initial release
---
+++ index.ts
@@ -0,0 +1,109 @@
+import * as path from 'path';
+import { generateTheme, IColorSet } from 'vscode-theme-generator';
+
+const colors = {
+  red: '#DA6771',
+  redLight: '#e5949b',
+  green: '#4EB071',
+  greenDim: '#275839',
+  yellow: '#fff099',
+  blue: '#399EF4',
+  blueLight: '#9fcff9',
+  pink: '#B168DF',
+  teal: '#21C5C7',
+  grey: '#535a6b',
+  accent: '#e0ed36',
+  accentDim: '#c3ce2f'
+}
+
+const colorSet: IColorSet = {
+  base: {
+    background: '#181818',
+    foreground: '#efefef',
+    color1: colors.accent,
+    color2: colors.red,
+    color3: colors.green,
+    color4: colors.yellow,
+  },
+  syntax: {
+    identifier: colors.blueLight,
+    string: colors.red,
+    number: colors.redLight,
+    keyword: colors.blue,
+    boolean: colors.blue,
+    function: colors.teal,
+    functionCall: colors.yellow,
+    storage: colors.blue,
+    comment: colors.grey,
+    class: colors.teal,
+    classMember: colors.teal,
+    type: colors.green,
+    cssClass: colors.blue,
+    cssId: colors.red,
+    cssTag: colors.teal,
+    markdownQuote: '#c0c0c0'
+  },
+  ui: {
+    cursor: '#ffffff',
+    guide: '#263040',
+    invisibles: '#263040',
+    rangeHighlight: '#263040',
+    // Bright red 50% opacity
+    findMatchHighlight: '#cb606080',
+    // Brighter red 50% opacity
+    currentFindMatchHighlight: '#ff777780',
+    selection: '#153958',
+    // Blue 50% opacity
+    selectionHighlight: '#2b74b380',
+    // White with ~10% opacity
+    wordHighlight: '#ffffff18',
+    wordHighlightStrong: '#ffffff18',
+    activeLinkForeground: colors.accent
+  },
+  terminal: {
+    black: '#666666',
+    red: colors.red,
+    green: colors.green,
+    yellow: colors.yellow,
+    blue: colors.blue,
+    magenta: colors.pink,
+    cyan: colors.teal,
+    white: '#efefef',
+    brightBlack: '#666666',
+    brightRed: colors.red,
+    brightGreen: colors.green,
+    brightYellow: colors.yellow,
+    brightBlue: colors.blue,
+    brightMagenta: colors.pink,
+    brightCyan: colors.teal,
+    brightWhite: '#efefef'
+  },
+  overrides: {
+    "diffEditor.insertedTextBackground": "#182818",
+    "diffEditor.removedTextBackground": "#261616",
+    "editorGutter.modifiedBackground": colors.green,
+    "editorGutter.addedBackground": "#399EF4",
+    "editorGutter.deletedBackground": colors.red,
+    "gitDecoration.modifiedResourceForeground": colors.green,
+    "gitDecoration.deletedResourceForeground": colors.red,
+    "gitDecoration.untrackedResourceForeground": "#399EF4",
+    "gitDecoration.conflictingResourceForeground": "#fff099",
+    "gitDecoration.ignoredResourceForeground": "#535a6b",
+    "editorError.foreground": colors.red,
+    "editorWhitespace.foreground": "#404040",
+    "editorIndentGuide.activeBackground": "#535353",
+    // "statusBarItem.remoteBackground": colors.accent,
+    // "statusBarItem.remoteForeground": "#000000",
+    // "badge.background": colors.accent,
+    // "badge.foreground": "#000000",
+    // "activityBarBadge.foreground": "#000000"
+  }
+};
+
+generateTheme('Sapphire', colorSet, path.join(__dirname, 'theme.json'));
+
+colorSet.base.background = '#242d3d'
+generateTheme('Sapphire (Bright)', colorSet, path.join(__dirname, 'theme-bright.json'));
+
+colorSet.base.background = '#080d14'
+generateTheme('Sapphire (Dim)', colorSet, path.join(__dirname, 'theme-dim.json'));
